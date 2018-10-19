9ed219525ec9d4bb45f0f3badba0d2100dc096ce
Daniel Kiss <dkiss@phase2technology.com>
2018-10-19 15:26:44 -0700

    add to foreach an associative array

--- for_each.php
+++ for_each.php
@@ -1,6 +1,6 @@
 <?php
   $first_string = empty($_GET["first_string"]) ? 0 : $_GET["first_string"];
-  $second_string = empty($_GET["first_string"]) ? 0 : $_GET["second_string"];
+  $second_string = empty($_GET["second_string"]) ? 0 : $_GET["second_string"];
   $third_string = empty($_GET["third_string"]) ? 0 : $_GET["third_string"];
   $fourth_string = empty($_GET["fourth_string"]) ? 0 : $_GET["fourth_string"];
   $fifth_string = empty($_GET["fifth_string"]) ? 0 : $_GET["fifth_string"];
@@ -13,4 +13,15 @@
   }
 
   echo 'total price is: '.$total_price;
+  $albums = array('album1' => 5, 'album2'=>6, 'album3'=>7, 'album4'=>8, 'album5'=>10);
 ?>
+
+<!--let's say you're in a band which has a catalog of 4 albums. Create a page in the browser to display each album with a price next to it. Use an associative array with a foreach loop.-->
+
+<ul>
+  <?php
+  foreach( $albums as $name => $price) {
+    echo "<li>" . "$name: $$price" . "</li>";
+  }
+  ?>
+</ul>
