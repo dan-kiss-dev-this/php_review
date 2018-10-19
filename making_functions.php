8eacb454ced97e6d0e3e89b5c2766fca07e6cbe7
Daniel Kiss <dkiss@phase2technology.com>
2018-10-19 11:10:56 -0700

    add branching statement

--- making_functions.php
+++ making_functions.php
@@ -4,9 +4,20 @@
 $string_one=$_GET["string_one"];
 $string_two=$_GET["string_two"];
 $string_three=$_GET["string_three"];
+$string_four=$_GET["string_four"];
 
 function playString($a, $b, $c) {
   return strrev($a) . ' '. strtoupper($b) . ' '. strrev(strtoupper($c)) ;
+}
+
+function tempTeller($temp)
+{
+    if ($temp > 80) {
+       return "It's too hot!";
+    } elseif ($temp <= 60) {
+       return "It's cold out!";
+    }
+    return "It's lovely out!";
 }
 ?>
 
@@ -24,6 +35,10 @@ function playString($a, $b, $c) {
   <!--Display the new string in the browser under the original phrases.--></p>
 <p>answer:
   <?php echo playString($string_one,$string_two,$string_three); ?>
+</p>
+<p>Create a website where the user enters a number for the current temperature outside, in fahrenheit. If the temperature is less than 60 degrees, show them the message "It's cold out!". If the temperature is over 80 degrees, show them the message "It's too hot!". Otherwise, display the message "It's lovely out!".</p>
+<p>answer to temp:
+  <?php echo tempTeller($string_four); ?>
 </p>
 </body>
 </html>
